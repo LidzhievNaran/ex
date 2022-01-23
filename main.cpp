891756661b2aa406720db474da7fa45948111c07
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 22:53:06 +0300

    task 18

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
-#include <math.h>
-
+#include <cstdlib>
+#include "ctime"
+#include <vector>
 int main() {
-    double k = pow(10, -4);
-    std::cout << k << std::endl;
-    double sum = 0;
-    double i = 1;
-    double x = i;
-    while(x>k){
-        x =(1/(i*(i+1)));
-        sum += x;
-        i++;
+    int n;
+    std::cout << "enter arr size"<< std::endl;
+    std::cin >> n;
+    std::vector<int> rand_arr(n);
+    srand(time(NULL));
+    for (int i =0; i < n; i++){
+        rand_arr[i] = 1+rand()%99;
     }
-    std::cout << i << std::endl;
-    std::cout << sum << std::endl;
+    std::vector<int> arr;
+    for (int i =0; i < n; i++){
+        std::cout << rand_arr[i] << std::endl;
+        if (rand_arr[i] % 3 == 0 && rand_arr[i] % 5 != 0){
+            arr.push_back(rand_arr[i]);
+        }
+    }
+    if (arr.empty()){
+        std::cout <<" there no numbers divisible by 3 and no divisible by 5" << std::endl;
+    }
+    for (int i = 0; i< arr.size();i++){
+        std::cout << arr[i] << std::endl;
+    }
 
 }
