e539d9e4078e5b21840a9215046090e7df498597
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 22:58:09 +0300

    task 19

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ int main() {
     std::vector<int> rand_arr(n);
     srand(time(NULL));
     for (int i =0; i < n; i++){
-        rand_arr[i] = 1+rand()%99;
+        rand_arr[i] = -49+rand()%99;
     }
     std::vector<int> arr;
     for (int i =0; i < n; i++){
         std::cout << rand_arr[i] << std::endl;
-        if (rand_arr[i] % 3 == 0 && rand_arr[i] % 5 != 0){
+        if (rand_arr[i] % 2 != 0 && rand_arr[i] < 0){
             arr.push_back(rand_arr[i]);
         }
     }
     if (arr.empty()){
-        std::cout <<" there no numbers divisible by 3 and no divisible by 5" << std::endl;
+        std::cout <<" there no numbers thah match the condition" << std::endl;
     }
     for (int i = 0; i< arr.size();i++){
         std::cout << arr[i] << std::endl;
