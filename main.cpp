e822232c2f8e27eedf2ffe5efcf293e1e9fe2865
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 20:02:39 +0300

    task5 idk(work like shit)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <math.h>
 int main() {
-    double x;
-    double y;
-    std::cout << "enter x"<< std::endl;
-    std::cin >> x;
-    std::cout << "enter y"<< std::endl;
-    std::cin >> y;
-    double h = sqrt(x*x + y*y);
-    std::cout << h << std::endl;
-    double r = 1;
-    double x1 = (-2+sqrt(4-4*(1-y)))/2;
-    double x2 = (-2-sqrt(4-4*(1-y)))/2;
-    std::cout << x1 << std::endl;
-    std::cout << x2 << std::endl;
-    if ( x >= x2 && x<=x1 && h<r){
-        std::cout << "the point belong to the shaded plane" << std::endl;
-    } else{
-        std::cout << "the point does not belong to the shaded plane" << std::endl;
-    }
+   double f,a,b;
+   int c;
+   int p =2;
+   std::cin>>a;
+   if (a>=0){
+       c= a/p;
+       b= a/p;
+       a = (b-c)*p;
+   }else{
+       if(a<0){
+        c =(-a)/p;
+        a = (c+1)*p+a;
+       }
+   }
+   f = pow(a,3)-5;
+   std::cout << f << std::endl;
 }
