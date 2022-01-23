4accbadbb3268d207afdc6be9242cdd641e0a0b9
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 22:00:42 +0300

    task11

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
+int summa(int arr[], int n){
+    int sum =0;
+    for (int i=0; i< n; i++){
+        sum+= arr[i];
+    }
+    return sum;
+}
 int main() {
-    double x;
-    double y;
-    std::cout << "enter x" << std::endl;
-    std::cin>> x;
-    std::cout << "enter y" << std::endl;
-    std::cin>> y;
-    double f;
-    if (x<y){
-        f = x-2*y+x*y;
-    } else{
-        if(x==y){
-            f = sin(x)*x;
-        } else{
-            f = y-2*x+1;
+    setlocale(LC_ALL, "Russia");
+    int n, m;
+    std::cout << "введите количество строк матрицы" << std::endl;
+    std::cin>> m;
+    std::cout << "введите количество столбцов матрицы" <<std::endl;
+    std::cin>> n;
+    int arr[m][n];
+    for (int i =0; i < m; i++){
+        for (int j =0; j < n; j++){
+            std::cout << "введите элемент ["<<i<<"] ["<<j<<"]"<< std::endl;
+            std::cin>> arr[i][j];
         }
     }
-    std::cout << f << std::endl;
+    int z ;
+    std::cout << "введите строку, которую хотите суммировать" << std::endl;
+    std::cin>> z;
+    int arr_f[n];
+    for (int i =0; i<m;i++){
+        arr_f[i] = arr[z][i];
+    }
+    std::cout<<"сумма строчки №"<< z << " = " << summa(arr_f, m) << std::endl;
 }
