e59c7e44952f5befd8b02a6d608bfb94e8b33643
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 19:45:51 +0300

    task4

--- main.cpp
+++ main.cpp
@@ -8,8 +8,13 @@ int main() {
     std::cout << "enter y"<< std::endl;
     std::cin >> y;
     double h = sqrt(x*x + y*y);
+    std::cout << h << std::endl;
     double r = 1;
-    if (x<=0 && x>=-1 && h < r){
+    double x1 = (-2+sqrt(4-4*(1-y)))/2;
+    double x2 = (-2-sqrt(4-4*(1-y)))/2;
+    std::cout << x1 << std::endl;
+    std::cout << x2 << std::endl;
+    if ( x >= x2 && x<=x1 && h<r){
         std::cout << "the point belong to the shaded plane" << std::endl;
     } else{
         std::cout << "the point does not belong to the shaded plane" << std::endl;
