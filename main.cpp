903f8cdfb25416324e58142ab7474ebff5f6af45
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-23 22:13:46 +0300

    task12

--- main.cpp
+++ main.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
 #include <vector>
-int summa(int arr[], int n){
-    int sum =0;
-    for (int i=0; i< n; i++){
-        sum+= arr[i];
-    }
-    return sum;
-}
+
 int main() {
-    setlocale(LC_ALL, "Russia");
-    int n, m;
-    std::cout << "введите количество строк матрицы" << std::endl;
-    std::cin>> m;
-    std::cout << "введите количество столбцов матрицы" <<std::endl;
-    std::cin>> n;
-    int arr[m][n];
-    for (int i =0; i < m; i++){
-        for (int j =0; j < n; j++){
-            std::cout << "введите элемент ["<<i<<"] ["<<j<<"]"<< std::endl;
-            std::cin>> arr[i][j];
-        }
-    }
-    int z ;
-    std::cout << "введите строку, которую хотите суммировать" << std::endl;
-    std::cin>> z;
-    int arr_f[n];
-    for (int i =0; i<m;i++){
-        arr_f[i] = arr[z][i];
-    }
-    std::cout<<"сумма строчки №"<< z << " = " << summa(arr_f, m) << std::endl;
+ int k;
+ std::cout << "enter a fibonacci end" << std::endl;
+ std::cin >> k;
+ int sum = 1;
+ int a = 1;
+ int b = 0;
+ int z;
+ for (int i =0; i< k-1; i++){//k-1 тк sum уже принила значение первого элемента равного единице
+     z = a;
+     a = a+b;
+     sum+=a;
+     b = z;
+ }
+ std::cout << sum << std::endl;
 }
